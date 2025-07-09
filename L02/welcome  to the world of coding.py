6c5f580719f24529cb76c411c0f5bbfdd44e6aba
AL1-457 <mohammedaliiftikhar@gmail.com>
2025-07-09 17:45:32 +0400

    L02

---
+++ L02/welcome  to the world of coding.py
@@ -0,0 +1,14 @@
+#Print a Sentence
+print("Welcome to the world of Programming!")
+
+#Print a number
+print(4)
+
+#Next Line after print
+print(" hello python")
+
+#pass multiple arguments for printing
+print("hello",5)
+
+# end argument of print statement
+print("welcome to ", end="*")
---
+++ understanding variables.py
@@ -0,0 +1,12 @@
+x=5
+ 
+y="Ali" 
+print(x)
+print(y)
+
+#user input
+name = input("enter your name:")
+print()
+
+print("hello " + name)
+print("welcome to codingal")
