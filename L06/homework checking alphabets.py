c5026fd8bc4de6cc27c76816430a1f53bdcaa8bb
AL1-457 <mohammedaliiftikhar@gmail.com>
2025-07-22 17:44:53 +0400

    L07

---
+++ L06/homework checking alphabets.py
@@ -0,0 +1,9 @@
+c = input("input(enter any character)")
+
+if c >= "a" and c <= "z":
+    print("it is Alphabet")
+elif c >= "A" and c <= "Z":
+    print("it is Alphabet")
+
+else:
+    print("it is not Alphabet")
