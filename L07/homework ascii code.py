e74da85552636721fdd4d94532429c2cfb5fca18
AL1-457 <mohammedaliiftikhar@gmail.com>
2025-07-23 17:43:32 +0400

    L08

---
+++ L07/homework ascii code.py
@@ -0,0 +1,106 @@
+c = input("input(enter any character)")
+
+if c == "A":
+    print("the ASCII code is 65")
+elif c == "a":
+    print("the ASCII code is 97")
+
+elif c == "B":
+    print("the ASCII code is 66")
+elif c == "b":
+    print("the ASCII code is 98")  
+
+if c == "C":
+    print("the ASCII code is 67")  
+elif c == "c":
+    print("the ASCII code is 99")  
+
+elif c == "D":
+    print("the ASCII code is 68")
+elif c == "d":
+    print("the ASCII code is 100")
+
+if c == "E":
+    print("the ASCII code is  69")
+elif c == "e":
+    print("the ASCII code is 101")
+
+elif c == "F":
+    print("the ASCII code is 70")
+elif c == "f":
+    print("the ASCII code is 102")
+
+if c == "G":
+    print("the ASCII code is 71")
+elif c == "g":
+    print("the ASCII code is 103")   
+
+if c == "H":
+    print("the ASCII code is 72")  
+elif c == "h":
+    print("the ASCII code is 104")     
+
+if c == "I":
+    print("the ASCII code is 73")
+elif c == "i":
+    print("the ASCII code is 105")  
+
+if c == "J":
+    print("the ASCII code is 74")  
+elif c == "j":
+    print("the ASCII code is 106") 
+
+if c == "K":
+    print("the ASCII code is 75")
+elif c == "k":
+    print("the ASCII code is 107")    
+
+if c == "L":
+    print("the ASCII code is 76")   
+elif c == "l":
+    print("the ASCII code is 108") 
+
+if c == "M":
+    print("the ASCII code is 77")
+elif c == "m":
+    print("the ASCII code is 109")
+
+if c == "N":
+    print("the ASCII code is 78")
+elif c =="n":
+    print("the ASCII code is 110")
+
+if c == "O":
+    print("the ASCII code is 79")
+elif c == "o":
+    print("the ASCII code is 111")
+
+if c == "P":
+    print("the ASCII code is 80")
+elif c == "p":
+    print("the ASCII code is 112")
+
+if c == "Q":
+    print("the ASCII code is 81")
+elif c == "q":
+    print("the ASCII code is 113")
+
+if c == "R":
+    print("the ASCII code is 82")
+elif c == "r":
+    print("the ASCII code is 114")
+
+if c == "S":
+    print("the ASCII code is 83")
+elif c == "s":
+    print("the ASCII code is 115")
+
+if c == "T":
+    print("the ASCII code is 84")
+elif c == "t":
+    print("the ASCII code is 116")
+
+if c == "U":
+    print("the ASCII code is 85")
+elif c == "u":
+    print("the ASCII code is 117")
