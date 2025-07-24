83c35f7a966b22f612b2bc63c241d8dec82c495b
AL1-457 <mohammedaliiftikhar@gmail.com>
2025-07-24 17:40:46 +0400

    L09

--- L07/homework ascii code.py
+++ L07/homework ascii code.py
@@ -10,7 +10,7 @@ elif c == "B":
 elif c == "b":
     print("the ASCII code is 98")  
 
-if c == "C":
+elif c == "C":
     print("the ASCII code is 67")  
 elif c == "c":
     print("the ASCII code is 99")  
@@ -20,7 +20,7 @@ elif c == "D":
 elif c == "d":
     print("the ASCII code is 100")
 
-if c == "E":
+elif c == "E":
     print("the ASCII code is  69")
 elif c == "e":
     print("the ASCII code is 101")
@@ -30,77 +30,102 @@ elif c == "F":
 elif c == "f":
     print("the ASCII code is 102")
 
-if c == "G":
+elif c == "G":
     print("the ASCII code is 71")
 elif c == "g":
     print("the ASCII code is 103")   
 
-if c == "H":
+elif c == "H":
     print("the ASCII code is 72")  
 elif c == "h":
     print("the ASCII code is 104")     
 
-if c == "I":
+elif c == "I":
     print("the ASCII code is 73")
 elif c == "i":
-    print("the ASCII code is 105")  
+    print("the ASCII code is 105")
 
-if c == "J":
+elif c == "J":
     print("the ASCII code is 74")  
 elif c == "j":
     print("the ASCII code is 106") 
 
-if c == "K":
+elif c == "K":
     print("the ASCII code is 75")
 elif c == "k":
     print("the ASCII code is 107")    
 
-if c == "L":
+elif c == "L":
     print("the ASCII code is 76")   
 elif c == "l":
     print("the ASCII code is 108") 
 
-if c == "M":
+elif c == "M":
     print("the ASCII code is 77")
 elif c == "m":
     print("the ASCII code is 109")
 
-if c == "N":
+elif c == "N":
     print("the ASCII code is 78")
 elif c =="n":
     print("the ASCII code is 110")
 
-if c == "O":
+elif c == "O":
     print("the ASCII code is 79")
 elif c == "o":
     print("the ASCII code is 111")
 
-if c == "P":
+elif c == "P":
     print("the ASCII code is 80")
 elif c == "p":
     print("the ASCII code is 112")
 
-if c == "Q":
+elif c == "Q":
     print("the ASCII code is 81")
 elif c == "q":
     print("the ASCII code is 113")
 
-if c == "R":
+elif c == "R":
     print("the ASCII code is 82")
 elif c == "r":
     print("the ASCII code is 114")
 
-if c == "S":
+elif c == "S":
     print("the ASCII code is 83")
 elif c == "s":
     print("the ASCII code is 115")
 
-if c == "T":
+elif c == "T":
     print("the ASCII code is 84")
 elif c == "t":
     print("the ASCII code is 116")
 
-if c == "U":
+elif c == "U":
     print("the ASCII code is 85")
 elif c == "u":
     print("the ASCII code is 117")
+
+elif c == "V":
+    print("the ASCII code is 86")
+elif c == "v":
+    print("the ASCII code is 118")
+
+elif c == "W":
+    print("the ASCII code is 87")
+elif c == "w":
+    print("the ASCII code is 119")
+
+elif c == "X":
+    print("the ASCII code is 88")
+elif c == "x":
+    print("the ASCII code is 120 ")
+
+elif c == "Y":
+    print("the ASCII code is 89")
+elif c == "y":
+    print("the ASCII code is 121")
+
+elif c == "Z":
+    print("the ASCII code is 90")
+elif c =="z":
+    print("the ASCII code is 122")
---
+++ L08/Home number swap.py
@@ -0,0 +1,17 @@
+a = 9
+b = 6
+c = 3
+
+print("original number", a)
+print("original number", b)
+print("original number", c)
+print()
+
+d = a
+a = b
+b = c
+c = d
+
+print("after number swap", a)
+print("after number swap", b)
+print("after number swap", c)
