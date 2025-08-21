0b426cc691923da0dcadad7ca8fc2b99822a522e
AL1-457 <mohammedaliiftikhar@gmail.com>
2025-08-21 17:48:56 +0400

    L21

---
+++ L21/L21 lists.py
@@ -0,0 +1,26 @@
+print("empty list:")
+empty_list = []
+print(empty_list)
+print()
+
+# A List of Numbers
+Numbers = [1, 2, 3, 4, 5]
+print("List of Numbers:")
+print(Numbers)
+print()
+
+# Use * Operator
+Triples = [5, 1, 7] * 3
+print("Triple List:")
+print(Triples)
+print()
+
+# reverse the given list
+aList = [100, 200, 300, 400, 500]
+print("Original List:")
+print(aList)
+print()
+
+aList = aList[::-1]
+print("List in reverse order:")
+print(aList)
