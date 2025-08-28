7bee130a97857f642dd9e93201beee20f5ed9abd
AL1-457 <mohammedaliiftikhar@gmail.com>
2025-08-28 17:41:15 +0400

    L24

---
+++ L23/L23 HW check frequency.py
@@ -0,0 +1,21 @@
+test_dict = {'Codingal' : 3,
+             'is' : 2,
+             'best' : 2,
+             'for' : 2,
+              'Coding' : 1}
+
+# printing original dictionary
+print("The original dictionary : " + str(test_dict))
+
+# Initialize value
+k = int(input("Enter the number to check frequency in Test dictionary : "))
+
+# Using loop
+# Selective key values in dictionary
+res = 0
+for key in test_dict:
+    if test_dict[key] == k:
+        res = res + 1
+
+# printing result
+print("Frequency of ",k," is : ",res) 
